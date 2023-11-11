f74abb118dfb5e9b8a1b5b3891053e10de40549a
Leonides2 <leonidesart@hotmail.com>
2023-11-11 10:11:08 -0600

    Base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CompanyModule } from './company/company.module';
+import { PhotoModule } from './photo/photo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [CompanyModule, PhotoModule,TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'nestproject',
+    entities:[Course, Student],
+    autoLoadEntities: true,
+    synchronize: true
+  }),
   controllers: [AppController],
   providers: [AppService],
 })
