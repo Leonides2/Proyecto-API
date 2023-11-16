972d5b8ebdcd413d300a3b7de9f19a026e5d09df
sharon1399 <svega1399@gmail.com>
2023-11-16 09:21:42 -0600

    reenviando commit 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello! Welcome to the resource api of the company. Please use a command api/[entity] to get the data';
   }
 }
